a3d62d993215a28d698b78e9ee087ef40b6426a4
jolabti <joparsip@gmail.com>
2016-12-08 19:01:39 +0700

    comit pertama

---
+++ app/src/main/java/com/apps/alter/pertemuan4/MainActivity.java
@@ -0,0 +1,19 @@
+package com.apps.alter.pertemuan4;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
